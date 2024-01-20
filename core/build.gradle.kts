8c21107753082e39504001bed0c0b8761e4e0171
haruki1125 <haruki1125@users.noreply.github.com>
2024-01-20 20:49:38 +0900

    coreをandroid.libraryに変更

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+    kotlin("kapt")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+android {
+    namespace = "com.example.core"
+}
+
+dependencies {
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-compiler:2.44")
 }
